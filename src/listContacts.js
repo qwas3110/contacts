727912f92386892e5d6aac767d86be0a642bbfc0
qwas3110 <283994917@qq.com>
2019-06-12 15:17:07 +0800

    Render default contacts

--- src/listContacts.js
+++ src/listContacts.js
@@ -5,9 +5,13 @@ class ListContacts extends Component {
     render() {
         console.log('Props:',this.props);
         return (
-            <div>
-                Laso
-            </div>
+            <ol className='contact-list'>
+                { this.props.contacts.map( (contact) => (
+                    <li key={contact.id}>
+                        {contact.name}
+                    </li>
+                )) }
+            </ol>
         );
     }
 }
