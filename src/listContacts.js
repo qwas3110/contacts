5fa41287d0af2f14f5a45bedeb8fcd13e2bd922a
qwas3110 <283994917@qq.com>
2019-06-12 14:56:42 +0800

    Add default contacts and the ListContacts component

---
+++ src/listContacts.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ListContacts extends Component {
+
+    render() {
+        console.log('Props:',this.props);
+        return (
+            <div>
+                Laso
+            </div>
+        );
+    }
+}
+
+
+export default ListContacts
