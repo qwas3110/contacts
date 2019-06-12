d3af66f12ae08238749c938dd703c3a006cf4a9c
qwas3110 <283994917@qq.com>
2019-06-12 18:36:32 +0800

    Hook up delete button to remove contacts

--- src/listContacts.js
+++ src/listContacts.js
@@ -15,7 +15,7 @@ function ListContacts(props) {
                         <p>{contact.name}</p>
                         <p>{contact.email}</p>
                     </div>
-                    <button className='contact-remove'>
+                    <button onClick={() => props.onDelectContact(contact)} className='contact-remove'>
                         remove
                     </button>
                 </li>
