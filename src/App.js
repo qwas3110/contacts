6d986c8d640e72c713301b4dbd584c250607ffa2
qwas3110 <283994917@qq.com>
2019-06-15 11:14:08 +0800

    生命周期事件 componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ListContacts from './listContacts';
-
-
+import * as ContactsAPI from './utils/ContactsAPI';
 
 
 
@@ -9,28 +8,17 @@ import ListContacts from './listContacts';
 
 class App extends Component {
   state = {
-      contacts: [
-          {
-              "id": "ryan",
-              "name": "Ryan Florence",
-              "email": "ryan@reacttraining.com",
-              "avatarURL": "http://localhost:5001/ryan.jpg"
-          },
-          {
-              "id": "michael",
-              "name": "Michael Jackson",
-              "email": "michael@reacttraining.com",
-              "avatarURL": "http://localhost:5001/michael.jpg"
-          },
-          {
-              "id": "tyler",
-              "name": "Tyler McGinnis",
-              "email": "tyler@reacttraining.com",
-              "avatarURL": "http://localhost:5001/tyler.jpg"
-          }
-      ]
+      contacts: []
   };
-  removeContact = (contact) => {
+
+  componentDidMount() {
+      ContactsAPI.getAll()
+          .then((contacts) => {
+              this.setState({contacts})
+          })
+  }
+
+    removeContact = (contact) => {
       this.setState((state) => ({
           contacts: state.contacts.filter((c) => c.id !== contact.id)
       }))
