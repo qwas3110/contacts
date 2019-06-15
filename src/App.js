2ba364dee7031710fa79e67ff71599a03e8c119f
qwas3110 <283994917@qq.com>
2019-06-15 15:30:15 +0800

    序列化新的联系人字段并传递给父组件

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
           })
   }
 
-    removeContact = (contact) => {
+  removeContact = (contact) => {
       this.setState((state) => ({
           contacts: state.contacts.filter((c) => c.id !== contact.id)
       }));
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
+import serialize from 'form-serialize'
+
 
 
 class CreateContact extends Component {
 
+    onSubmitCreate = (e) => {
+        e.preventDefault();
+        const value = serialize(e.target, {hash:true});
+        if (this.props.onCreateContact) {
+            this.props.onCreateContact(value)
+        }
+    };
+
+
     render() {
         return (
             <div>
-                CreateContact
+                <Link className='close-create-contact'
+                      to='/'
+                />
+                <form className='create-contact-form'
+                      onSubmit={this.onSubmitCreate}
+                      >
+                    <ImageInput className='create-contact-avatar-input'
+                                name='avatarURL'
+                                maxHeight={64}
+                    />
+                    <div className='create-contact-details'>
+                        <input type="text" name='name' placeholder='Name'/>
+                        <input type="text" name='email' placeholder='Email'/>
+                        <button>Add Contact</button>
+                    </div>
+                </form>
+
             </div>
         );
     }
